f1a9d0be19c65d00b609277ec7739d6cf54ea7e9
nj-anderson <norahjanderson@gmail.com>
2026-09-18 12:24:32 -0400

    Implemented basic page navigation.

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+
+// NAV BAR
+document.getElementById("navbar").innerHTML = `
+    <nav>
+        <a href="/" class="logo">Find Gamers</a>
+
+        <div class="nav-links">
+            <a href="/explore">Explore</a>
+            <a href="/profile">Profile</a>
+            <a href="/connections">Connections</a>
+        </div>
+    </nav>
+`;
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve CSS, JavaScript, and images
+app.use(express.static(path.join(__dirname, "public")));
+
+// Pages
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/explore", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "explore.html"));
+});
+
+app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "profile.html"));
+});
+
+app.get("/connections", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "connections.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
